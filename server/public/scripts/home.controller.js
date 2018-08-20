689edd1097f8e7b06eec8ab3c0701596d92a80ed
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-19 21:32:36 -0500

    Separated controllers to separate files. Comments and cleanup.

---
+++ server/public/scripts/home.controller.js
@@ -0,0 +1,47 @@
+const images = [ // objects to use in add listing form image select
+  {name: 'Select Image', path: ''},
+  {name: 'Classic Flats', path: 'classic-flats.jpg'},
+  {name: 'Haunted', path: 'haunted.png'},
+  {name: 'Older', path: 'older.jpg'},
+  {name: 'Rental', path: 'rental.jpg'},
+  {name: 'Rental 2', path: 'rental2.jpg'},
+  {name: 'Shiny', path: 'shiny.jpg'},
+  {name: 'Stoney', path: 'stoney.jpg'}
+];
+
+app.controller('HomeController', function($http) {
+  //console.log('in HomeController');
+  vm = this;
+
+  vm.images = images;
+
+  vm.newListing = { // form inputs object
+    cost: '',
+    sqft: '',
+    type: 'rent',
+    city: '',
+    image_path: ''
+  };
+
+  vm.addToListings = function() {
+    if(!Object.values(vm.newListing).every(value => value)) { // if every form input has not been completed
+      console.log('must fill all form fields');
+      return;
+    }
+
+    $http({
+      method: 'POST',
+      url: '/listings',
+      data: vm.newListing
+    }).then(response => {
+      console.log('/listings POST success:', response);
+      vm.newListing = { // clear inputs
+        cost: '',
+        sqft: '',
+        type: 'rent',
+        city: '',
+        image_path: ''
+      };
+    }).catch(error => console.log('/listings POST error:', error));
+  }; // end addToListings
+}); // end HomeController
---
+++ server/public/scripts/listing.controller.js
@@ -0,0 +1,71 @@
+app.controller('ListingsController', function($http, $location) {
+  const pageURL = $location.url(); // url is always '/listings/rent' or '/listings/sale'
+  vm = this;
+
+  vm.listings = [];
+
+  vm.deleteListing = function(id){
+    $http({
+      method: 'DELETE',
+      url: '/listings/' + id
+    }).then(response => {
+      console.log('/listings DELETE success:', response);
+      getListings();
+    })
+    .catch(error => console.log('/listings DELETE error:', error));
+  }; // end deleteListing
+
+  vm.searchListings = function(search) {
+    if (search.term === 'city' && !search.value) { // if name is blank for city, get all listings
+      getListings();
+      return;
+    } else if ((search.term === 'cost' || search.term === 'sqft') && (!search.min && !search.max)) { // if both min and max are blank, get all listings
+      getListings();
+      return;
+    }
+
+    const searchPath = generateSearchPath(search);
+
+    $http({
+      method: 'GET',
+      url: pageURL + searchPath
+    }).then(function(response) {
+      console.log('search response:', response.data);
+      vm.listings = response.data;
+    }).catch(function(error) {
+      console.log('/listings search error:', error);
+    });
+  } // end searchListings
+
+  function generateSearchPath(search) {
+    let searchPath = '/search';
+
+    // complete search path based on what is inside search object
+    // for city -> '/search/city/:partialName'
+    // for cost or sqft -> '/search/cost?min=[INT]&max=[INT]'
+    if (search.term === 'city') {
+      searchPath += '/city/' + search.value;
+    } else if (search.term === 'cost' || search.term === 'sqft') {
+      searchPath += `/${search.term}?`;
+      if (search.min && !search.max) {
+        searchPath += 'min=' + search.min;
+      } else if (!search.min && search.max) {
+        searchPath += 'max=' + search.max;
+      } else if (search.min && search.max) {
+        searchPath += `min=${search.min}&max=${search.max}`;
+      } 
+    }
+    return searchPath;
+  } // end generateSearchPath
+
+  function getListings(){
+    $http({
+      method: 'GET',
+      url: pageURL
+    }).then(response => {
+      vm.listings = response.data;
+    }).catch(error => console.log('getListings error:', error));
+  }; // end getListings
+
+  getListings(); // get all listings on page load
+}); // end ListingsController
