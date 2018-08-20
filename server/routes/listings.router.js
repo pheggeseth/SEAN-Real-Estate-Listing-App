3649c43187b8dfb64a4f56b684c391f4b4dfc0d1
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-20 16:33:49 -0500

    Added pool.js module for Heroku.

--- server/routes/listings.router.js
+++ server/routes/listings.router.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const pg = require('pg');
-const Pool = pg.Pool;
-const config = {
-  database: 'real_estate', // name of database
-  host: 'localhost',
-  port: 5432,
-  max: 10, // max number of concurrent connections
-  idleTimeoutMillis: 10000 // attepmt to connect for 10 seconds
-};
-
-const pool = new Pool(config);
-pool.on('connect', () => console.log('postgresql connected!!!'));
-pool.on('error', error => console.log('Error connecting to db', error));
-
+const pool = require('../modules/pool.js');
 
 // GET ALL OF EITHER RENT OR SALE
 router.get('/:type', (req, res) => {
